daa80c1d347afa6528f16349ff588a106cab1bae
Anand-1442 <anandsingh14442@gmail.com>
2024-06-05 13:32:29 +0530

    first commit

---
+++ src/components/design/Benefits.jsx
@@ -0,0 +1,5 @@
+export const GradientLight = () => {
+  return (
+    <div className="absolute top-0 left-1/4 w-full aspect-square bg-radial-gradient from-[#8e2b666b] to-[#457300c7]/0 to-70% pointer-events-none" />
+  );
+};
